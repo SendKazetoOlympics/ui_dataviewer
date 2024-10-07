81139adf9f68cfe0045d8515e6b501be68968d1b
Kaze Wong <kazewong.physics@gmail.com>
2024-10-06 23:19:49 -0400

    Add function to convert opencv frame to ndarray

--- src/video_reader.rs
+++ src/video_reader.rs
@@ -1,8 +1,27 @@
-use opencv::{highgui, prelude::*, videoio, Result};
+use ndarray::ArrayView3;
+use opencv::{prelude::*, videoio, Result};
+
+trait AsArray {
+    fn try_as_array(&self) -> Result<ArrayView3<u8>>;
+}
+impl AsArray for Mat {
+    fn try_as_array(&self) -> Result<ArrayView3<u8>> {
+        // if !self.is_continuous() {
+        //     return Err(anyhow!("Mat is not continuous"));
+        // }
+        let bytes = self.data_bytes()?;
+        let size = self.size()?;
+        let a =
+            ArrayView3::from_shape((size.height as usize, size.width as usize, 3), bytes).unwrap();
+        Ok(a)
+    }
+}
 
 pub fn video_to_frames(file_name: String) {
     let mut file = videoio::VideoCapture::from_file(&file_name, videoio::CAP_ANY).unwrap();
     let mut frame = Mat::default();
     file.read(&mut frame).unwrap();
     println!("Reading frame {:?}", frame);
+    let array = frame.try_as_array().unwrap();
+    println!("Reading array {:?}", array);
 }
