c473d36f52a9ebd8b6162c3ab64ac52a3c32468c
Kaze Wong <kazewong.physics@gmail.com>
2024-10-07 23:32:29 -0400

    array can be used now, but rerun seems to have hiccup regarding image shape

--- src/bin/read_video.rs
+++ src/bin/read_video.rs
@@ -1,4 +1,9 @@
+use std::borrow::Borrow;
+
 use clap::Parser;
+use ndarray::{s, Array, ShapeBuilder};
+use opencv::core::MatTraitConstManual;
+use rerun::TensorData;
 use ui_dataviewer::video_reader::video_to_frames;
 
 #[derive(Parser)]
@@ -6,9 +11,20 @@ struct Cli {
     file_name: String,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
+    let rec = rerun::RecordingStreamBuilder::new("SKO_inspector").spawn()?;
     println!("Reading file with name {:?}", args.file_name);
-    video_to_frames(args.file_name);
+    let array: TensorData = video_to_frames(args.file_name)
+        .as_slice()
+        .unwrap()
+        .try_into()
+        .unwrap();
+    rec.log(
+        "image",
+        &rerun::Image::from_color_model_and_tensor(rerun::ColorModel::RGB, array)?,
+    )?;
+
+    Ok(())
 }
